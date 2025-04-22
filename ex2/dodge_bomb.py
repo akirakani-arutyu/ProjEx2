da72a7fca863897a52360194867da057a28fe43c
takashi <c0a24254@edu.teu.ac.jp>
2025-04-22 17:31:29 +0900

    演習3途中

--- ex2/dodge_bomb.py
+++ ex2/dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import random
 import sys
+import time
 import pygame as pg
 
 
@@ -33,6 +34,8 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    kk_img_r = pg.Surface((20,20))
+    kk_img_r = kk_img
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
 
@@ -40,17 +43,19 @@ def main():
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)#正方形に円を描く
     bb_rct = bb_img.get_rect()#画面に貼付
     bb_rct.center = random.randint(0,WIDTH), random.randint(0,HEIGHT)#貼付場所指定
-    
-    bb_img.set_colorkey((0,0,0))
+    bb_img.set_colorkey((0,0,0))#黒消す
     vx, vy = 5, 5
 
     clock = pg.time.Clock()
-    tmr = 0
+    tmr = 1
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])
+
+        if kk_rct.colliderect(bb_rct):#鳥と爆弾が重なってたら
+            gameover(screen)
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -58,23 +63,60 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+                if sum_mv[0] > 0:
+                    kk_img_r = pg.transform.flip(kk_img,True,False)
+                    if sum_mv[1] > 0:
+                        kk_img_r = pg.transform.rotate(kk_img_r,315)
+                    elif sum_mv[0] < 0:
+                        kk_img_r = pg.transform.rotate(kk_img_r,45)
+                elif sum_mv[0] < 0:
+                    kk_img_r = kk_img
+                    if sum_mv[1] > 0:
+                        kk_img_r = pg.transform.rotate(kk_img_r,315)
+                    
+
         kk_rct.move_ip(sum_mv)
 
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)
+
+        screen.blit(kk_img_r, kk_rct)
 
         bb_rct.move_ip(vx,vy)#爆弾移動
         x, y =  check_bound(bb_rct)
         if not x:
             vx *= -1
         if not y:
-            vy *= -1
+            vy *= -1 #壁で反転
         screen.blit(bb_img, bb_rct)#爆弾描画
+
+        if tmr % 431 == 0:
+            vx *= tmr / 350
+            vy *= tmr / 350 #球の加速
+            bb_img = pg.Surface((20 * tmr / 200, 20 * tmr / 200))
+            pg.draw.circle(bb_img, (255, 0, 0), (10*tmr/200, 10*tmr/200), 10*tmr/200)
+            bb_img.set_colorkey((0,0,0)) #球の拡大
 
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
+
+def gameover(screen: pg.Surface) -> None:
+    """
+    引数:screen
+    戻り値:画面
+    """
+    go_img = pg.transform.rotozoom(pg.image.load("fig/6.png"), 0, 10)
+    go_txt = pg.font.Font(None, 100).render("Game over", True, (255,0,0))
+    go_surface = pg.Surface((WIDTH,HEIGHT))
+    pg.draw.circle(go_surface, (0,0,0),(100,100),10)
+    pg.Surface.set_alpha(go_surface, 150, 0)
+    screen.blit(go_surface,[0,0])#黒背景表示
+    screen.blit(go_img,[100,100])#鳥表示
+    screen.blit(go_txt, [400,200])#文字表示
+    pg.display.update()
+    time.sleep(5)
 
 
 if __name__ == "__main__":
